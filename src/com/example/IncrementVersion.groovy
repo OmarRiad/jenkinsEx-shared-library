0f427963adf8121d51e35a5b04e6c98dff286ef2
Omar Riad <omarriad22@gmail.com>
2024-05-08 04:34:43 +0300

    update IncrementVersion

--- src/com/example/IncrementVersion.groovy
+++ src/com/example/IncrementVersion.groovy
@@ -11,7 +11,7 @@ class IncrementVersion {
         def packageJson = script.readJSON file:'package.json'
         def version = packageJson.version
         script.echo "$version"
-        env.IMAGE_NAME = "$version-$env.BUILD_NUMBER"
-        script.echo "$env.IMAGE_NAME"
+        script.env.IMAGE_NAME = "$version-$script.env.BUILD_NUMBER"
+        script.echo "$script.env.IMAGE_NAME"
     }
 }
